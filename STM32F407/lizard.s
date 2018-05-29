6249dfb86206aa5661e5ae32c7c763c79207e94a
Ivan Kozlov <ivan.klaipeda@gmail.com>
2018-05-29 17:19:23 +0200

    [master]: base setup of an .s file for Lizard streamcipher

--- STM32F407/lizard.s
+++ STM32F407/lizard.s
@@ -1,55 +1,183 @@
-
 .syntax unified
 .cpu cortex-m4
 
-.global power_5_13
-.type power_5_13, %function
-power_5_13:
+.global lizard
+.type lizard, %function
+lizard:
     # Remember the ABI: we must not destroy the values in r4 to r12.
     # Arguments are placed in r0 and r1, the return value should go in r0.
     # To be certain, we just push all of them onto the stack.
     push {r4-r12}
-
-    # Exercise 1 a)
-    # Write a function power_5_13() to compute 5 ** 13 (i.e. 5 to the power 13)
-    mov r0, #5
-    mov r1, #5
-    mul r0, r0
-    mul r0, r0
-    mul r0, r0
-    mul r0, r1
-    mul r0, r1
-    mul r0, r1
-    mul r0, r1
-    mul r0, r1
+    #TODO
     # Finally, we restore the callee-saved register values and branch back.
     pop {r4-r12}
     bx lr
 
-.global power
-.type power, %function
-power:
+.global loadkey
+.type loadkey, %function
+loadkey:
     # Remember the ABI: we must not destroy the values in r4 to r12.
     # Arguments are placed in r0 and r1, the return value should go in r0.
     # To be certain, we just push all of them onto the stack.
     push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
 
-    # Exercise 1 b)
-    # Write a function power(x, y) that computes x ** y (i.e. x to the power y)
-    mov r2, r0
-    mov r3, #1
-loop:    
+.global loadIV
+.type loadIV, %function
+loadIV:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global initRegisters
+.type initRegisters, %function
+initRegisters:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global mixing
+.type mixing, %function
+mixing:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global keyadd
+.type keyadd, %function
+keyadd:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global diffusion
+.type diffusion, %function
+diffusion:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global NFSR1
+.type NFSR1, %function
+NFSR1:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global NFSR2
+.type NFSR2, %function
+NFSR2:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global construct
+.type construct, %function
+construct:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global initialization
+.type initialization, %function
+initialization:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global keystreamGeneration
+.type keystreamGeneration, %function
+keystreamGeneration:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global keystreamGenerationSpecification
+.type keystreamGenerationSpecification, %function
+keystreamGenerationSpecification:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global a
+.type a, %function
+a:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
+
+.global a
+.type a, %function
+a:
+    # Remember the ABI: we must not destroy the values in r4 to r12.
+    # Arguments are placed in r0 and r1, the return value should go in r0.
+    # To be certain, we just push all of them onto the stack.
+    push {r4-r12}
+    #TODO
+    # Finally, we restore the callee-saved register values and branch back.
+    pop {r4-r12}
+    bx lr
     
-
-
-    cmp r1, 0
-
-    mov r0, r3
-    mul r3, r2
-    sub r1, #1
-    bne loop 
-
-
-    # Finally, we restore the callee-saved register values and branch back.
-    pop {r4-r12}
-    bx lr
